02fa459bd5834eb617abad126634fb5a856d682b
yimmyflorez <yimmyflorez16@hotmail.com>
2024-08-11 17:29:04 +1000

    version 1

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# project6
-Construir y desplegar un panel de control de una aplicación web en un servicio en la nube.
+# Aplicación Web de Análisis de Vehículos
+
+Esta aplicación web permite visualizar y analizar datos de anuncios de venta de coches. Fue desarrollada como parte de un proyecto para practicar tareas habituales de la ingeniería de software, incluyendo la creación y gestión de entornos virtuales de Python, el desarrollo de una aplicación web y su despliegue en la nube.
+
+## Funcionalidad
+
+La aplicación proporciona las siguientes funcionalidades:
+
+- **Histograma del Odómetro**: Visualiza la distribución del kilometraje de los vehículos en el conjunto de datos.
+- **Diagrama de Dispersión**: Muestra la relación entre el precio de los vehículos y su kilometraje.
+
+## Cómo Ejecutar la Aplicación
+
+1. Clona este repositorio.
+2. Instala las dependencias utilizando `pip install -r requirements.txt`.
+3. Ejecuta la aplicación con `streamlit run app.py`.
+
+## Despliegue
+
+La aplicación está desplegada en Render y puede ser accesible públicamente 
+
+## Datos
+
+El conjunto de datos utilizado en esta aplicación proviene de anuncios de venta de coches y está disponible en la plataforma TripleTen para Data scientist 
+
+## Autor
+
+Desarrollado por Yimmy Serrano F.
---
+++ app.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import streamlit as st
+import plotly_express as px
+
+# Leer los datos
+car_data = pd.read_csv('/Users/yimmyserrano/Documents/vehicles_env/project6/vehicles_us.csv')
+
+# Título de la aplicación
+st.title("Aplicación Web de Análisis de Vehículos")
+
+# Descripción de la aplicación
+st.markdown("""
+Esta aplicación web permite visualizar y analizar datos de anuncios de venta de coches.
+Selecciona las opciones a continuación para mostrar un histograma del odómetro o un diagrama de dispersión de precio vs odómetro.
+""")
+
+# Crear casillas de verificación para el histograma y el diagrama de dispersión
+hist_checkbox = st.checkbox('Mostrar histograma')
+disp_checkbox = st.checkbox('Mostrar diagrama de dispersión')
+
+# Si se selecciona la casilla de verificación del histograma
+if hist_checkbox:
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    
+    # Crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+    
+    # Mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+# Si se selecciona la casilla de verificación del diagrama de dispersión
+if disp_checkbox:
+    st.write('Creación de un diagrama de dispersión para el conjunto de datos de anuncios de venta de coches')
+    
+    # Crear el diagrama de dispersión
+    fig2 = px.scatter(car_data, x="odometer", y="price")
+    
+    # Mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig2, use_container_width=True)
